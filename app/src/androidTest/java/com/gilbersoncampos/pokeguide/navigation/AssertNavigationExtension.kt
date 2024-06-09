f16ddb5906360ca1cc55e1e88a28a9b044eceb65
Gilberson Campos <gra.campos@discent.ufma.br>
2024-06-09 18:10:01 -0300

    feat:remove extension navigation

--- app/src/androidTest/java/com/gilbersoncampos/pokeguide/navigation/AssertNavigationExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.gilbersoncampos.pokeguide.navigation
-import androidx.navigation.NavController
-import org.junit.Assert
-
-fun NavController.assertCurrentRoute(expectedRoute:String){
-    Assert.assertEquals(expectedRoute,currentBackStackEntry?.destination?.route)
-}
